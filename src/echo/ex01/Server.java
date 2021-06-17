8f07ad09a98bc7175a6bdf3fa4789e9f5640f36e
서종오 <84363963+SeoJongO@users.noreply.github.com>
2021-06-17 15:56:43 +0900

    0617 6교시

--- src/echo/ex01/Server.java
+++ src/echo/ex01/Server.java
@@ -8,6 +8,20 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 
 		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress("192.168.0.149", 10001));
+		
+		System.out.println("<서버시작>");
+		System.out.println("===================================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+		
+		Socket socket = serverSocket.accept();
+		
+		System.out.println("[클라이언트가 연결되었습니다.]");
+		
+		
+		
+		socket.close();
+		serverSocket.close();
 		
 
 	}
