eb90c85134c84923967f3bc6742fe5549d4d4d05
SeoJongO <eve7884dbqls@naver.com>
2021-06-18 11:21:56 +0900

    네트워크 통신

--- src/echo/ex01/Server.java
+++ src/echo/ex01/Server.java
@@ -1,14 +1,52 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
 	public static void main(String[] args) throws IOException {
 
 		ServerSocket serverSocket = new ServerSocket();
-		
+		serverSocket.bind(new InetSocketAddress("192.168.35.177", 10001));
+
+		System.out.println("<서버시작>");
+		System.out.println("===================================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+
+		Socket socket = serverSocket.accept();
+
+		System.out.println("[클라이언트가 연결되었습니다.]");
+
+		// 받기용
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		String msg = br.readLine();
+		System.out.println("받은 메시지:" + msg);
+
+		// 보내기용
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		bw.write(msg);
+		bw.newLine();
+		bw.flush();
+
+		bw.close();
+		br.close();
+		socket.close();
+		serverSocket.close();
 
 	}
 
