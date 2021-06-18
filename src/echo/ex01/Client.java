ea7ee0754647691da8a613fe0cb70c65d5a7b896
서종오 <84363963+SeoJongO@users.noreply.github.com>
2021-06-18 12:36:58 +0900

    Update Client.java

--- src/echo/ex01/Client.java
+++ src/echo/ex01/Client.java
@@ -1,25 +1,68 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		Socket socket = new Socket();
-		
+
 		System.out.println("<클라이언트 시작>");
 		System.out.println("==================================");
 		System.out.println("[서버에 연결을 요청합니다.]");
-		
-		socket.connect(new InetSocketAddress("ip", 10001));
-		
+
+		socket.connect(new InetSocketAddress("192.168.35.177", 10001));
+
 		System.out.println("[서버에 연결되었습니다.]");
+
+		// 보내기용
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		// 받기용
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		// 보내기
+		Scanner sc = new Scanner(System.in);
 		
+		while (true) {
+
+			String str = sc.nextLine();
+
+			bw.write(str);
+			if ("/q".equals(str)) {
+				System.out.println("[접속 종료되었습니다.]");
+				break;
+			}
+			bw.newLine();
+			bw.flush();
+
+			String reMsg = br.readLine();
+			System.out.println("Server:[" + reMsg + "]");
+
+		}
+
+		System.out.println("===================================");
+		System.out.println("<클라이언트 종료>");
+		
+		sc.close();
+		br.close();
+		bw.close();
 		socket.close();
-		
+
 	}
 
 }
